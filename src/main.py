8fd9139f0ba28e7ffe75bc0a1bf9a48fcaf6e659
Salvador Guzman <guzmansalv@gmail.com>
2017-11-18 22:51:21 -0800

    finished implementing solution

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 import requests
+from operator import itemgetter
 
 r = requests.get('https://leetcode.com/api/problems/all/')
 json = r.json()
-print(json)
+statList = json['stat_status_pairs']
+stats = [x['stat'] for x in statList]
+
+problemSet = [(x['question_id'], x['question__title']) for x in stats]
+sort = sorted(problemSet)
+maxStrLen = max([len(x[-1]) for x in sort])
+
+for p in sort:
+    formatStr = "LeetCode {:>3}. {}"
+    formatMe = formatStr.format(p[0], p[1])
+    print(formatMe)
